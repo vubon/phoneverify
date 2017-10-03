5af3b84d87f20b875fb57973a3cc74aa04bc5e38
vubon <vubon.roy@gmail.com>
2017-10-03 16:56:04 +0600

    Testing signals in models.py

--- verify/models.py
+++ verify/models.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import signals
+# from django.db.models.signals import post_delete
 
 # Create your models here.
+
+
+def create_msg(sender, created, instance,**kwargs):
+    print('Your Post has been created ')
+
+
+def delete_msg(sender, instance, **kwargs):
+    print('Your post has been deleted')
+
+
+class Test_person(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+signals.post_save.connect(create_msg, sender=Test_person)
+
+signals.post_delete.connect(delete_msg, sender=Test_person)
+
--- verify/views.py
+++ verify/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from twilio.rest import Client
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def test(request):
+    # put your own credentials here
+    account_sid = "ACb1198191b353896cd4cedfe0bee18788"
+    auth_token = "426fe34ea601d9c9a169679d44b0dc16"
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to="+8801737388296",
+        from_="+15005550006",
+        body="Tomorrow's forecast in Financial District, San Francisco is Clear",
+        media_url="https://climacons.herokuapp.com/clear.png")
+
+    print('Message Sent', message.sid)
+
+    return render(request, 'test.html')
