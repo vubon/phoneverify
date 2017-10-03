fc80f376a5bf7df4207fcab58bdfcc397ce907d7
vubon <vubon.roy@gmail.com>
2017-10-03 18:05:27 +0600

    done

--- verify/models.py
+++ verify/models.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.db.models import signals
-# from django.db.models.signals import post_delete
-
+from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -23,8 +24,17 @@ class Test_person(models.Model):
     def __str__(self):
         return self.name
 
+# Todo: Need to test  @receiver([post_save, post_delete], sender=User)
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s %s" %(self.first_name, self.last_name)
+
 
 signals.post_save.connect(create_msg, sender=Test_person)
-
+signals.post_save.connect(create_msg, sender=Person)
 signals.post_delete.connect(delete_msg, sender=Test_person)
+signals.post_delete.connect(delete_msg, sender=Person)
 
